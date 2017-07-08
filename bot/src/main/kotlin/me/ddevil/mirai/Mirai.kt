fc14a52d06580d014cab9a4aca7abb9a0326a51f
DDevil_ <brunorbsf@gmail.com>
2017-07-08 10:34:32 -0400

    - Basic event system

--- bot/src/main/kotlin/me/ddevil/mirai/Mirai.kt
+++ bot/src/main/kotlin/me/ddevil/mirai/Mirai.kt
@@ -1,5 +1,5 @@
 package me.ddevil.mirai
 
-class Mirai{
+class Mirai {
 
 }
---
+++ bot/src/main/kotlin/me/ddevil/mirai/event/EventManager.kt
@@ -0,0 +1,44 @@
+package me.ddevil.mirai.event
+
+import com.google.common.collect.HashMultimap
+import com.google.common.collect.Multimap
+import com.google.common.eventbus.EventBus
+import me.ddevil.mirai.plugin.Plugin
+import me.ddevil.util.minusAssign
+
+class EventManager {
+    companion object {
+        const val eventBusName = "MiraiEventBus"
+    }
+
+    private val eventBus = EventBus(eventBusName)
+    private val registeredListeners: Multimap<Plugin, Any> = HashMultimap.create()
+
+    fun unregisterListener(listener: Any) {
+        registeredListeners -= listener
+        eventBus.unregister(listener)
+    }
+
+    fun unregisterListeners(plugin: Plugin) {
+        val listeners = registeredListeners[plugin]
+        for (listener in listeners) {
+            eventBus.unregister(listener)
+        }
+        registeredListeners.removeAll(plugin)
+    }
+
+    fun registerListener(plugin: Plugin, listener: Any) {
+        registeredListeners.put(plugin, listener)
+        eventBus.register(listener)
+    }
+
+    fun call(event: Event) {
+        eventBus.post(event)
+    }
+}
+
+class Event {
+    fun call(manager: EventManager) {
+        manager.call(this)
+    }
+}
