f618e40de7c1878f9fb0a50ba65bc6a30a23c3b4
DDevil_ <brunorbsf@gmail.com>
2018-02-01 02:30:04 -0300

    - Why did we add .idea? - Added deploy command

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if [ -f $1 ]
+then
+    rm -rf $1
+fi
+
+mkdir $1
+mkdir $1/plugins
+echo "Copying files to $1"
+
+cp mirai.sh $1/mirai.sh
+cp output/bot/libs/bot-all.jar $1/mirai.jar
+
+for D in output/plugins/*; do
+    if [ -d "${D}" ]; then
+        projectName=${D#output/plugins/}
+        fileName=${projectName}-all.jar
+        filePath="output/plugins/${projectName}/libs/${fileName}"
+        if [ ! -f ${filePath} ]
+        then
+            fileName=${projectName}.jar
+             filePath="output/plugins/${projectName}/libs/${fileName}"
+        fi
+        echo "Copying plugin ${projectName} with file ${fileName}"
+        cp  ${filePath} "$1/plugins/${projectName}.jar"  # your processing here
+    fi
+done
+ch u+x $1/mirai.sh
+(cd $1; $1/mirai.sh start)
---
+++ mirai.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PID=mirai.pid
+
+start() {
+    if [ -f ${PID} ]
+    then
+        echo
+        echo "Mirai is started @ $( cat ${PID} )"
+    else
+        if nohup java -jar mirai.jar 2>&1 &
+        then
+            pid=$!
+            echo "Mirai starting @ $pid"
+            echo ${pid} >${PID}
+        else
+            echo "An error has occoured"
+            rm ${PID}
+        fi
+    fi
+}
+
+stop() {
+    if [ -f ${PID} ]
+    then
+        pid=$(cat ${PID})
+        kill -9 ${pid}
+        rm ${PID}
+        echo "Mirai was stopped @ ${pid}!"
+    else
+        echo "Mirai is not running!"
+    fi
+}
+
+$1
+if [ $? = 127 ]
+then
+    echo "Error: '$1' is not a known subcommand."
+    exit 1
+fi
